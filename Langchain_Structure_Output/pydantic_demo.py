4e811198a6437edd6553367d9b30b43fc47104c0
Krishn Jatav <164301111+krishn1122@users.noreply.github.com>
2026-05-26 12:13:48 +0000

    pydantic_all

--- Langchain_Structure_Output/pydantic_demo.py
+++ Langchain_Structure_Output/pydantic_demo.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr, Field
 from typing import Optional
 
 class student(BaseModel):
-    name: str ='Krishna' # default_value
+    name: str ='Krishna Jatav' # default_value
     age: Optional[int] = None # optional field
-new_student = { "age": 25 }
+    email: EmailStr # required field with email validation
+    cgpa: float=Field(gt=0.0, lt=10.0, default=5.0, description="Cumulative Grade Point Average") # required field with validation for cgpa between 0 and 10
+new_student = { "age": 24, "email": "krishna@example.com", "cgpa": 8.5 }
 student=student(**new_student)
-print(student)
+
+student_dic=dict(student)
+print(student_dic)
+
+student_json=student.model_dump_json()
+print(student_json)
